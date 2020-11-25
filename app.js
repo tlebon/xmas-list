9d302cebff38ae3d0ac7c8f26cc0be3dc45fa698
Timothy LeBon <tlebon@gmail.com>
2020-11-25 23:53:39 +0100

    feat: Added two comparison functions I made two simple comparison functions, using switch statements. I opted for this over a ternary for readability although considering they are a binary, I may switch to a ternary after. I wanted to destructure the dino as well since I thought it was cleaner.

--- app.js
+++ app.js
@@ -44,9 +44,33 @@ btn.addEventListener("click", pullForm);
 
 // Create Dino Compare Method 1
 // NOTE: Weight in JSON file is in lbs, height in inches.
+function heightCompare(human, dino) {
+	const humHeight = human.feet * 12 + human.inches;
+	const [height, species] = dino;
+
+	const comparison = height - humHeight;
+	switch (humHeight < height) {
+		case true:
+			return `Wow, you are only ${comparison} inches shorter than a ${species}!`;
+		case false:
+			return `Wait, that can't be right- you are ${-comparison} inches taller than a ${species}!`;
+	}
+}
 
 // Create Dino Compare Method 2
 // NOTE: Weight in JSON file is in lbs, height in inches.
+function weightCompare(human, dino) {
+	const humWeight = human.weight;
+	const [weight, species] = dino;
+
+	const comparison = weight - humWeight;
+	switch (humWeight < weight) {
+		case true:
+			return `Wow, you are ${comparison} pounds lighter than a ${species}! Do you do crossfit or something?`;
+		case false:
+			return `Wait, that can't be right- you are ${-comparison} lbs heavier than a ${species}! Maybe lay off the burgers for a while`;
+	}
+}
 
 // Create Dino Compare Method 3
 // NOTE: Weight in JSON file is in lbs, height in inches.
