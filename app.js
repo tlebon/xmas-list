43d3b6cde10b9a047a5fb428fcaecc4b292c7fbb
Timothy LeBon <tlebon@gmail.com>
2020-11-25 23:23:56 +0100

    feat: Adding human constructor and initial form pull This is just an initial idea for how to get the form data. Seems to work but it isnt an IIFE currently. I just wanted a way to take the value names and create a simple object, I went with a reducer for now.

--- app.js
+++ app.js
@@ -1,25 +1,46 @@
 // Create Dino Constructor
 function Dinosaur(species, weight, height, diet, where, when, fact) {
-  this.species = species;
-  this.weight = weight;
-  this.height = height;
-  this.diet = diet;
-  this.where = where;
-  this.when = when;
-  this.fact = fact;
+	this.species = species;
+	this.weight = weight;
+	this.height = height;
+	this.diet = diet;
+	this.where = where;
+	this.when = when;
+	this.fact = fact;
 }
 
 // Create Dino Objects
 let dinos = [];
-let getDinos= (async () => {
-  let response = await fetch("./dino.json");
-  let dinojson = await response.json()
-  .then(data=> dinos= data.Dinos.map(dino=>new Dinosaur(dino)));
+let getDinos = (async () => {
+	let response = await fetch("./dino.json");
+	let dinojson = await response
+		.json()
+		.then((data) => (dinos = data.Dinos.map((dino) => new Dinosaur(dino))));
 })();
 
 // Create Human Object
-
+function Human(name, weight, feet, inches, diet) {
+	this.name = name;
+	this.weight = weight;
+	this.feet = feet;
+	this.inches = inches;
+	this.diet = diet;
+}
 // Use IIFE to get human data from form
+let human;
+
+function pullForm() {
+	const humanAttr = ["name", "weight", "feet", "inches", "diet"];
+	const humanEl = humanAttr.reduce((acc, stat) => {
+		acc[stat] = document.getElementById(stat).value;
+		return acc;
+	}, {});
+	// return humanEl;
+	human = Human(humanEl);
+	console.log(human);
+}
+const btn = document.getElementById("btn");
+btn.addEventListener("click", pullForm);
 
 // Create Dino Compare Method 1
 // NOTE: Weight in JSON file is in lbs, height in inches.
